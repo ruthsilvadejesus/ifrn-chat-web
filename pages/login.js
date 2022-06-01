19c07a077dd7d98b0023624556ee893ec183dcb5
ruthsilvadejesus <ruthdejesus3101@gmail.com>
2022-06-01 17:37:38 -0300

    alteração no login e adição do index

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { AppButton,AppContainer, AppInput } from '../src/components';
 
 const Login = props => {
+  const router = useRouter();
+
   return (
     <div>
-      <div>
-        <label>Usuário: </label>
-        <input type="text" title="apelido ou email do usuário" />
+      <Head>
+        <title>página de login</title>
+      </Head>
+      <main>
+        <nav>
+          <p>Chat de Infoweb</p>
+          <button onClick={() => router.back()}>voltar</button>
+        </nav>
+        <AppContainer>
+          <AppInput label="Usuário" title="apelido ou email do usuário"/>
+          <AppInput label="Senha" type="password" />
+          <AppButton title="Clique aqui para entrar no chat" label="Entrar no chat" />
+        </AppContainer>
+      </main>
       </div>
-      <div>
-        <label>Senha: </label>
-        <input type="password" title="senha do usuário" />
-      </div>
-      <div>
-        <input type="submit" title="Entrar" />
-      </div>
-    </div>
   );
 };
 
